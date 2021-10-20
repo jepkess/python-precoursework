040891fac6216ae12bf6385dece232c9bc2b525e
jepkess <biivincent1997@gmail.com>
2021-10-20 19:42:53 +0300

    creating a  testing file

---
+++ contact_test.py
@@ -0,0 +1,12 @@
+import unittest # import the unittest module
+from contact import contact # importing the contact from the contact class
+
+class TestContact(unittest.TestCase):
+    """
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+
+    """
+    
