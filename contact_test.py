dcb216dc8b40e287e765663d7fbeb737aaccdc75
jepkess <biivincent1997@gmail.com>
2021-10-21 09:34:44 +0300

    savin the contact using the save.cntact() method

--- contact_test.py
+++ contact_test.py
@@ -10,6 +10,7 @@ class TestContact(unittest.TestCase):
 
     """
 #items up here 
+#creating a contact
 def setUp(self):
     """
     set up the test to run before each test cases
@@ -19,10 +20,37 @@ def test_init_contact(self):
     """
     test_init test case to test if the object is initialized properly
     """
-    self.assertEqual(self.new_contact.first_name,"James")
-    self.assertEqual(self.new_contact.last_name,"Muriuki")
-    self.assertEqual(self.new_contact.number,"0712345678")
-    self.assertEqual(self.new_contact.email,"james@ms.com")
+    self.assertEqual(self.new_contact.first_name,"James")# self.assertEqual checks for the an expected results
+    self.assertEqual(self.new_contact.last_name,"Muriuki")# self.assertEqual checks for the an expected results
+    self.assertEqual(self.new_contact.number,"0712345678")# self.assertEqual checks for the an expected results
+    self.assertEqual(self.new_contact.email,"james@ms.com")# self.assertEqual checks for the an expected results
 
 if __name__=="__main__":  
-    unittest.main() 
+    unittest.main() #provides a command line interface that collects all the tests methods and executes them.
+
+#saving a contact
+
+def test_save_contact(self):
+    """
+     test_save_contact test case to test if the contact object is saved into
+         the contact list
+    """
+      
+    self.new_contact.save_contact()#saving a new contact
+    self.assertEqual(len(Contact.contact_list), 1)
+
+if __name__ == '__main__':
+    unittest.main()  
+
+#saving the contact
+def test_save_contact(self):
+    """
+     test_save_contact test case to test if the contact object is saved into
+         the contact list
+    """
+    self.new_contact.save_contact()#saving a new contact
+    self.assertEqual(len(Contact.contact_list),1)
+if __name__=='__main__':
+    unittest.main()
+
+
